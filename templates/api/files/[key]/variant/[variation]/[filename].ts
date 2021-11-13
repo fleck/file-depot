a5c5949faf2646c5caab394af3852aee01571bad
Jonathan <jonfleck88@gmail.com>
2021-11-12 20:17:39 -0500

    update filename

--- templates/api/files/[key]/variant/[variation]/[filename].ts
+++ templates/api/files/[key]/variant/[variation]/[filename].ts
@@ -1,53 +1,74 @@
-import { disk } from "app/file/disk"
-import { BlitzApiRequest, BlitzApiResponse, NotFoundError } from "blitz"
-import db from "db"
-import createContentDisposition from "content-disposition"
-import path from "path"
-import { localPath } from "app/api/files/create"
-import { validTransformKey, transformFunctions } from "../../../../../file/transforms"
+import { disk } from "app/file/disk";
+import { BlitzApiRequest, BlitzApiResponse, NotFoundError } from "blitz";
+import db from "db";
+import createContentDisposition from "content-disposition";
+import path from "path";
+import { localPath } from "app/api/files/create";
+import {
+  validTransformKey,
+  transformFunctions,
+  Transform,
+} from "../../../../../file/transforms";
+import crypto from "crypto";
 
-export const variantPath = (hash: string, variation: string) =>
-  path.join("variant", hash.substring(0, 2), hash.substring(2, 4), variation)
+export const variantPath = (key: string, variation: Transform) =>
+  path.join(
+    "variant",
+    key.substring(0, 2),
+    key.substring(2, 4),
+    /** Use the functions source as a key for the transform then it'll update if the implementation changes  */
+    crypto
+      .createHash("sha1")
+      .update(transformFunctions[variation].toString())
+      .digest("hex")
+  );
 
-export default async function handler(req: BlitzApiRequest, res: BlitzApiResponse) {
-  const { key, contentDisposition, variation } = req.query
+export default async function handler(
+  req: BlitzApiRequest,
+  res: BlitzApiResponse
+) {
+  const { key, contentDisposition, variation } = req.query;
 
   if (!key || key instanceof Array) {
-    throw new NotFoundError()
+    throw new NotFoundError();
   }
 
   if (typeof variation !== "string" || !validTransformKey(variation)) {
-    throw new NotFoundError("invalid variation")
+    throw new NotFoundError("invalid variation");
   }
 
-  const file = await db.file.findFirst({ where: { key } })
+  const file = await db.file.findFirst({ where: { key } });
 
-  if (!file) throw new NotFoundError()
+  if (!file) throw new NotFoundError();
 
   if (file.contentType) {
-    res.setHeader("content-type", file.contentType)
+    res.setHeader("content-type", file.contentType);
   }
 
   res.setHeader(
     "content-disposition",
     createContentDisposition(file.name, {
       type: contentDisposition === "attachment" ? "attachment" : "inline",
-    }),
-  )
+    })
+  );
 
-  res.setHeader("Cache-Control", "public, max-age=31536000")
+  res.setHeader("Cache-Control", "public, max-age=31536000");
 
-  const existingVariant = disk.getStream(variantPath(file.key, variation))
+  const existingVariant = disk.getStream(variantPath(file.key, variation));
 
   existingVariant
     .on("error", function createVariant() {
-      const originalFile = disk.getStream(localPath(file.key))
+      const originalFile = disk.getStream(localPath(file.key));
 
-      transformFunctions[variation](originalFile).pipe(res)
+      const transformedFile = transformFunctions[variation](originalFile);
+
+      disk.put(variantPath(file.key, variation), transformedFile);
+
+      transformedFile.pipe(res);
 
       originalFile.on("error", function noVariantOrOriginalFileIsFound() {
-        res.statusCode = 404
-      })
+        res.statusCode = 404;
+      });
     })
-    .pipe(res)
+    .pipe(res);
 }
