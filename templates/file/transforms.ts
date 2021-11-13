1e2b0508cb6a2875a5e657db6da85d81f865be86
Jonathan <jonfleck88@gmail.com>
2021-11-12 20:21:37 -0500

    update transform

--- templates/file/transforms.ts
+++ templates/file/transforms.ts
@@ -1,9 +1,10 @@
-import sharp from "sharp"
+import sharp from "sharp";
 
 export const transformFunctions = {
-  rotate: (file: NodeJS.ReadableStream) => file.pipe(sharp().rotate()),
-}
+  rotate: (file: NodeJS.ReadableStream) => file.pipe(sharp().rotate(90)),
+};
 
-export type Transform = keyof typeof transformFunctions
+export type Transform = keyof typeof transformFunctions;
 
-export const validTransformKey = (key: string): key is Transform => key in transformFunctions
+export const validTransformKey = (key: string): key is Transform =>
+  key in transformFunctions;
